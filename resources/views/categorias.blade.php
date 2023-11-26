80428ddabb10218caf3a1b1eb3f46f47a6798756
RFunes <rodrigofunes@hotmail.es>
2023-11-26 15:09:07 -0600

    26/11/23 - Cambios a la vista categoria

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -6,103 +6,141 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Landing Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/444ab6c87d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/categorias.css') }}">
-    <script src="https://kit.fontawesome.com/444ab6c87d.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </head>
 
-<body>
+<body>    
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">SoftBytes</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">                
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Buscar" aria-label="Buscar" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-light" type="button" id="button-addon2">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="input-group" style="margin: 0 15px;">
+                    <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Categoria 1</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria 2</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria 3</a></li>
+                    </ul>
+                    <input type="text" class="form-control" placeholder="Buscar" aria-label="Buscar"
+                        aria-describedby="button-addon2">
+                    <button class="btn btn-outline-light" type="
+                    button" id="button-addon2">
                         <i class="fa-solid fa-magnifying-glass fa-sm" style="color: #ffffff;"></i>
                     </button>
                 </div>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 15px;">                    
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Categorias
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Categoria 1</a></li>
-                            <li><a class="dropdown-item" href="#">Categoria 2</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Categoria 3</a></li>
-                        </ul>
-                    </li>
-                </ul>
+
                 <!--
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
-                -->
+                
                 <div style="margin: 15px;">
                     <i class="fa-solid fa-cart-shopping" style="color: white;"></i>
+                </div>
+                -->
+                <div class="acciones">
+                    <span class="fa-stack fa-layers fa-fw fa-1x" style="--fa-inverse: #F7EDE4;">
+                        <i class="fa-solid fa-circle fa-stack-2x" style="color: #195E95;"></i>
+                        <i class="fa-solid fa-cart-shopping fa-stack-1x fa-inverse" data-bs-toggle="offcanvas"
+                            data-bs-target="#cestaCompra"></i>
+                        <span id="numeroCompra">3</span>
+                    </span>
                 </div>
             </div>
         </div>
     </nav>
     <div class="contenedor">
         <div class="contenido">
-            <div class="card" style="width: 12rem; height: 12rem;">
-                <div class="imagen-categoria">
-                    <img src="..." class="card-img-top" alt="...">
-                </div>
+            <div class="card" style="width: 12rem; height: 15rem; background-image: url('https://th-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c1_opp_maokong_27_shellwhite_t_nt_has_captaincrunchwireless_win11_coreset_front_camup_whitebg_intel.png');">
                 <div class="card-body">
                     <a href="#" class="btn btn-primary">Categoria 1</a>
                 </div>
             </div>
-            <div class="card" style="width: 12rem; height: 12rem;">
-                <div class="imagen-categoria">
-                    <img src="..." class="card-img-top" alt="...">
-                </div>
+            <div class="card" style="width: 12rem; height: 15rem; background-image: url('https://th-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c1_opp_maokong_27_shellwhite_t_nt_has_captaincrunchwireless_win11_coreset_front_camup_whitebg_intel.png');">
                 <div class="card-body">
-                    <a href="#" class="btn btn-primary">Categoria 1</a>
+                    <a href="#" class="btn btn-primary">Categoria 2</a>
                 </div>
             </div>
-            <div class="card" style="width: 12rem; height: 12rem;">
-                <div class="imagen-categoria">
-                    <img src="..." class="card-img-top" alt="...">
-                </div>
+            <div class="card" style="width: 12rem; height: 15rem; background-image: url('https://th-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c1_opp_maokong_27_shellwhite_t_nt_has_captaincrunchwireless_win11_coreset_front_camup_whitebg_intel.png');">
                 <div class="card-body">
-                    <a href="#" class="btn btn-primary">Categoria 1</a>
+                    <a href="#" class="btn btn-primary">Categoria 3</a>
                 </div>
             </div>
-            <div class="card" style="width: 12rem; height: 12rem;">
-                <div class="imagen-categoria">
-                    <img src="..." class="card-img-top" alt="...">
-                </div>
+            <div class="card" style="width: 12rem; height: 15rem; background-image: url('https://th-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c1_opp_maokong_27_shellwhite_t_nt_has_captaincrunchwireless_win11_coreset_front_camup_whitebg_intel.png');">
                 <div class="card-body">
-                    <a href="#" class="btn btn-primary">Categoria 1</a>
+                    <a href="#" class="btn btn-primary">Categoria 4</a>
                 </div>
             </div>
-            <div class="card" style="width: 12rem; height: 12rem;">
-                <div class="imagen-categoria">
-                    <img src="..." class="card-img-top" alt="...">
-                </div>
+            <div class="card" style="width: 12rem; height: 15rem; background-image: url('');">
                 <div class="card-body">
-                    <a href="#" class="btn btn-primary">Categoria 1</a>
+                    <a href="#" class="btn btn-primary">Categoria 5</a>
                 </div>
             </div>
+            
         </div>
     </div>
 
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="cestaCompra" aria-labelledby="cestaCompraLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="cestaCompraLabel">Carrito de Compras</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="fondo-linea">
+                <div id="pedidos">
 
+                </div>
+                <form>
+                    <div class="row">
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaD">Total:</label>
+                        </div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaT" id="totalCompra">Lps. 0.00</label>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaD">Impuesto:</label>
+                        </div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaT" id="impuestoCompra">Lps. 0.00</label>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaD">Comisión</label>
+                        </div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaT" id="comisionCompra">Lps. 0.00</label>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaD">Total a Pagar</label>
+                        </div>
+                        <div class="col-4">
+                            <label class="form-label cuentaCestaT" id="totalPagar">Lps. 0.00</label>
+                        </div>
+                    </div>
 
+                    <div class="d-flex mb-2 mt-2" style="justify-content: center">
+                        <button type="button" class="btn btn-danger texto-general boton" data-bs-dismiss="offcanvas"
+                            onclick="mostrarEnvio();">Continuar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
